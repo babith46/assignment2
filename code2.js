b918111056bb55ca4b80edfbfd3823c590093a14
babith46 <112344827+babith46@users.noreply.github.com>
2023-09-11 19:48:41 +0530

    Add files via upload

---
+++ code2.js
@@ -0,0 +1,9 @@
+function string_val() {
+    return "Hello, World!";
+  }
+  
+  // Call the function and store the result in a variable
+  const myString = string_val();
+  
+  // Display the result
+  console.log(myString);
